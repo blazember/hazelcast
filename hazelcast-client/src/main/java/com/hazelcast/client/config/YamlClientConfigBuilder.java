c3e144eed135372cf74fc467fdcc475b80831b79
Zoltan Baranyi <blazember@gmail.com>
2019-02-08 11:44:45 +0100

    wip

---
+++ hazelcast-client/src/main/java/com/hazelcast/client/config/YamlClientConfigBuilder.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright (c) 2008-2019, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hazelcast.client.config;
+
+import com.hazelcast.config.ConfigLoader;
+import com.hazelcast.config.InvalidConfigurationException;
+import com.hazelcast.internal.yaml.YamlLoader;
+import com.hazelcast.internal.yaml.YamlMapping;
+import com.hazelcast.internal.yaml.YamlNode;
+import com.hazelcast.logging.ILogger;
+import com.hazelcast.logging.Logger;
+import com.hazelcast.nio.IOUtil;
+import com.hazelcast.util.ExceptionUtil;
+import org.w3c.dom.Node;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import static com.hazelcast.config.yaml.W3cDomUtil.asW3cNode;
+
+public class YamlClientConfigBuilder {
+
+    private static final ILogger LOGGER = Logger.getLogger(YamlClientConfigBuilder.class);
+
+    private final InputStream in;
+
+    public YamlClientConfigBuilder(String resource) throws IOException {
+        URL url = ConfigLoader.locateConfig(resource);
+        if (url == null) {
+            throw new IllegalArgumentException("Could not load " + resource);
+        }
+        this.in = url.openStream();
+    }
+
+    public YamlClientConfigBuilder(File file) throws IOException {
+        if (file == null) {
+            throw new NullPointerException("File is null!");
+        }
+        this.in = new FileInputStream(file);
+    }
+
+    public YamlClientConfigBuilder(URL url) throws IOException {
+        if (url == null) {
+            throw new NullPointerException("URL is null!");
+        }
+        this.in = url.openStream();
+    }
+
+    public YamlClientConfigBuilder(InputStream in) {
+        this.in = in;
+    }
+
+    /**
+     * Loads the client config using the following resolution mechanism:
+     * <ol>
+     * <li>first it checks if a system property 'hazelcast.client.config' is set. If it exist and it begins with
+     * 'classpath:', then a classpath resource is loaded. Else it will assume it is a file reference</li>
+     * <li>it checks if a hazelcast-client.Yaml is available in the working dir</li>
+     * <li>it checks if a hazelcast-client.Yaml is available on the classpath</li>
+     * <li>it loads the hazelcast-client-default.Yaml</li>
+     * </ol>
+     */
+    public YamlClientConfigBuilder() {
+        YamlClientConfigLocator locator = new YamlClientConfigLocator();
+        this.in = locator.getIn();
+    }
+
+    public ClientConfig build() {
+        return build(Thread.currentThread().getContextClassLoader());
+    }
+
+    public ClientConfig build(ClassLoader classLoader) {
+        ClientConfig clientConfig = new ClientConfig();
+        build(clientConfig, classLoader);
+        return clientConfig;
+    }
+
+    void build(ClientConfig clientConfig, ClassLoader classLoader) {
+        clientConfig.setClassLoader(classLoader);
+        try {
+            parseAndBuildConfig(clientConfig);
+        } catch (Exception e) {
+            throw ExceptionUtil.rethrow(e);
+        } finally {
+            IOUtil.closeResource(in);
+        }
+    }
+
+    private void parseAndBuildConfig(ClientConfig config) throws Exception {
+        YamlMapping yamlRootNode;
+        try {
+            yamlRootNode = ((YamlMapping) YamlLoader.load(in));
+        } catch (Exception ex) {
+            throw new InvalidConfigurationException("Invalid YAML configuration", ex);
+        }
+
+        YamlNode imdgRoot = yamlRootNode.childAsMapping(ClientConfigSections.HAZELCAST_CLIENT.name.toLowerCase());
+        if (imdgRoot == null) {
+            throw new InvalidConfigurationException("No mapping with hazelcast-client key is found in the provided "
+                    + "configuration");
+        }
+
+        Node w3cRootNode = asW3cNode(imdgRoot);
+        //        replaceVariables(w3cRootNode);
+        //        importDocuments(imdgRoot);
+        //
+        //        new YamlMemberDomConfigProcessor(true, config).buildConfig(w3cRootNode);
+    }
+
+}

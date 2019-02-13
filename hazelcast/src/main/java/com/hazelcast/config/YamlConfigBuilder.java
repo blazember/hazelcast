9a7a6cf156b478ef134653ce04df1d7701ea7468
Zoltan Baranyi <blazember@gmail.com>
2019-02-13 23:23:10 +0100

    Test using properties provided to XML and YAML Config instances

--- hazelcast/src/main/java/com/hazelcast/config/YamlConfigBuilder.java
+++ hazelcast/src/main/java/com/hazelcast/config/YamlConfigBuilder.java
@@ -280,8 +280,9 @@ public class YamlConfigBuilder implements ConfigBuilder {
         return replacer;
     }
 
-    public void setProperties(Properties properties) {
+    public YamlConfigBuilder setProperties(Properties properties) {
         this.properties = properties;
+        return this;
     }
 
     private void fillReplacerProperties(Node node, Properties properties) {
